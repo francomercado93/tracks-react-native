95155a0be4a2672fc78338f7653e14334cfef301
francomercado93 <mercadofranco.a@gmail.com>
2020-02-19 12:56:30 -0300

    Add functions to AuthContext

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Spacer from '../components/Spacer';
 import { Text, Input, Button } from 'react-native-elements';
-
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+
+    const { state, signup } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    console.log(state);
+
     return (<View style={styles.container}>
         <Spacer>
             <Text h3>Sign Up for Tracker</Text>
         </Spacer>
-        <Input label="Email" />
+        <Input
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
         <Spacer />
-        <Input label="Password" />
+        <Input
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry
+        />
+        {state.errorMessage ?
+            (<Text style={styles.errorMessage}>{state.errorMessage}</Text>)
+            : null}
         <Spacer>
-            <Button title="Sign Up" />
+            <Button
+                title="Sign Up"
+                onPress={() => signup({ email, password })} />
         </Spacer>
     </View>)
 }
@@ -29,7 +54,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        marginBottom: 200
+        marginBottom: 200,
+        marginHorizontal: 15
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        alignSelf: 'center',
+        marginTop: 15
     }
 });
 
